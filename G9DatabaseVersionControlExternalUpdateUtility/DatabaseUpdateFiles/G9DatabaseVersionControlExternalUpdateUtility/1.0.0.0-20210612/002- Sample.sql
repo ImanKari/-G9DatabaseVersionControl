8d0c812bd6feb95db70c5ba2684aed66f1a19170
Iman Kari <iman.kari@live.com>
2021-06-12 13:39:57 +0430

    Fixed

--- G9DatabaseVersionControlExternalUpdateUtility/DatabaseUpdateFiles/G9DatabaseVersionControlExternalUpdateUtility/1.0.0.0-20210612/002- Sample.sql
+++
@@ -1,5 +0,0 @@
--- Note: Do not use this file in any way, this file will be replaced with new content in updates.
--- <Author>Iman Kari</Author>
--- <Description>Updated view for fixing problems in task number 226</Description>
--- <UpdateDateTime>1990-09-01 12:45:00</UpdateDateTime>
-Select 1 --What you want done!
