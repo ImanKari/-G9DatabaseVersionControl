7eb0add33c6c03b88ca1f4cf7d1e2ca2abfb0fc9
Iman Kari <iman.kari@live.com>
2021-07-16 23:40:41 +0430

    Fixed default logger Separate web content from the core Add Web output ready project to manage database version Fixed connection string drop down in web output Support multi custom task Add choose multi task in UI

---
+++ G9DatabaseVersionControlCore/DataType/G9DtCustomTask.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace G9DatabaseVersionControlCore.DataType
+{
+    /// <summary>
+    ///     Data type fore custom task
+    /// </summary>
+    public readonly struct G9DtCustomTask
+    {
+        #region ### Fields And Properties ###
+
+        /// <summary>
+        ///     Specifies nickname of custom task
+        /// </summary>
+        public readonly string Nickname;
+
+        /// <summary>
+        ///     Specifies description of custom task
+        /// </summary>
+        public readonly string Description;
+
+        /// <summary>
+        ///     Func for execute custom task
+        ///     <para />
+        ///     <para />
+        ///     First param: string => Specifies database name (Can't be null)
+        ///     <para />
+        ///     Second param: Action(string) => Access to action for execute a query on database without result
+        ///     <para />
+        ///     Third param: Fun(Dictionary(string, object)) => Access to func for execute a query on database with result (List
+        ///     specifies rows and dictionary specifies column and value)
+        ///     <para />
+        ///     Fourth param: Specifies task is successful or no (func answer)
+        /// </summary>
+        public readonly Func<string, Action<string>, Func<string, List<Dictionary<string, object>>>, G9DtTaskResult>
+            CustomTaskFunc;
+
+        #endregion
+
+        #region ### Methods ###
+
+        /// <summary>
+        ///     Constructor - Initialize Requirement
+        /// </summary>
+        /// <param name="nickname">Specifies nickname of custom task</param>
+        /// <param name="description">Specifies description of custom task</param>
+        /// <param name="customTaskFunc">
+        ///     Func for execute custom task
+        ///     <para />
+        ///     <para />
+        ///     First param: string => Specifies database name (Can't be null)
+        ///     <para />
+        ///     Second param: Action(string) => Access to action for execute a query on database without result
+        ///     <para />
+        ///     Third param: Fun(Dictionary(string, object)) => Access to func for execute a query on database with result (List
+        ///     specifies rows and dictionary specifies column and value)
+        ///     <para />
+        ///     Fourth param: Specifies task is successful or no (func answer)
+        /// </param>
+        public G9DtCustomTask(string nickname, string description,
+            Func<string, Action<string>, Func<string, List<Dictionary<string, object>>>, G9DtTaskResult> customTaskFunc)
+        {
+            Nickname = nickname;
+            Description = description;
+            CustomTaskFunc = customTaskFunc;
+        }
+
+        #endregion
+    }
+}
