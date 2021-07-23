e18b2a8930c8394bd2a42fe0285f0869a75961c5
Iman Kari <iman.kari@live.com>
2021-07-23 21:19:21 +0430

    -- Add pre-build custom validation check -- Add custom targets generator for (Windows and linux)

--- G9DatabaseVersionControlSampleUpdate/Program.cs
+++ G9DatabaseVersionControlSampleUpdate/Program.cs
@@ -6,6 +6,7 @@ namespace G9DatabaseVersionControlSampleUpdate
     {
         private static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
